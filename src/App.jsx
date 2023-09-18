1ffd4c15cb1ec3d23d35f6d8b8d7e75990c831a5
Antonio Saracino <antonio.saracino83@gmail.com>
2023-09-18 12:24:27 +0200

    Added functional components

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,36 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 const ParentComponent = () => {
+  const [bankMoney, setBankMoney] = useState(1000);
+
+  const handleBuyToy = (toyPrice) => {
+    setBankMoney(prevBankMoney => prevBankMoney - toyPrice);
+  };
 
   return (
     <div>
       <h1>Dad</h1>
+      <h2>Bank Account: ${bankMoney}</h2>
+      <ToyComponent toyName="Lego" priceTag="$20" onBuy={handleBuyToy} />
+      <ToyComponent toyName="Transformers" priceTag="$45" onBuy={handleBuyToy} />
     </div>
-  )
-  
+  );
 }
 
 
-const ChildComponent = () => {
-
+const ToyComponent = ({ toyName, priceTag, onBuy }) => {
+  const handleBuyClick = () => {
+    const toyPrice = parseInt(priceTag.slice(1), 10);
+    onBuy(toyPrice);
+  };
 
   return (
-    <div>
-      <h1>Kid</h1>
+    <div className='Item'>
+      <h2>{toyName}</h2>
+      <h3>{priceTag}</h3>
+      <button onClick={handleBuyClick}>Buy</button>
     </div>
-  )
+  );
 }
 
 
@@ -30,7 +42,6 @@ function App() {
   return (
     <div className='App'>
       <ParentComponent />
-      <ChildComponent />
 
     </div>
   )
